8d7abca3af69ac6a3af5a17fa16ae0e250d1e3d1
Scott Chamberlain <myrmecocystus@gmail.com>
2016-11-01 11:33:51 -0700

    added progress option for non-parllel runs

--- R/isd_parser.R
+++ R/isd_parser.R
@@ -5,6 +5,9 @@
 #' @param parallel (logical). do processing in parallel. Default: \code{FALSE}
 #' @param cores (integer) number of cores to use: Default: 2. We look in
 #' your option "cl.cores", but use default value if not found.
+#' @param progress (logical) print progress - ignored if \code{parallel=TRUE}.
+#' The default is \code{FALSE} because printing progress adds a small bit of
+#' time, so if processing time is important, then keep as \code{FALSE}
 #' @references ftp://ftp.ncdc.noaa.gov/pub/data/noaa
 #' @return A tibble (data.frame)
 #' @examples \dontrun{
@@ -12,14 +15,17 @@
 #'
 #' (res <- isd_parse(path))
 #'
+#' # with progress
+#' (res2 <- isd_parse(path, progress = TRUE))
+#'
 #' # in parallel
 #' (out <- isd_parse(path, parallel = TRUE))
 #'
 #' identical(res, out)
-#'
-#' download.file()
 #' }
-isd_parse <- function(path, parallel = FALSE, cores = getOption("cl.cores", 2)) {
+isd_parse <- function(path, parallel = FALSE, cores = getOption("cl.cores", 2),
+                      progress = FALSE) {
+
   if (!file.exists(path)) stop("file not found", call. = FALSE)
   message(sprintf("<path>%s", path), "\n")
 
@@ -29,7 +35,16 @@ isd_parse <- function(path, parallel = FALSE, cores = getOption("cl.cores", 2)) 
     linesproc <- parallel::parLapply(cl, lns, each_line, sections = sections)
     parallel::stopCluster(cl)
   } else {
-    linesproc <- lapply(lns, each_line, sections = sections)
+    #linesproc <- lapply(lns, each_line, sections = sections)
+    linesproc <- vector(mode = "list", length = length(lns))
+    if (progress) {
+      pb <- utils::txtProgressBar(min = 0, max = length(lns), initial = 0, style = 3)
+      on.exit(close(pb))
+    }
+    for (i in seq_along(lns)) {
+      if (progress) utils::setTxtProgressBar(pb, i)
+      linesproc[[i]] <- each_line(lns[i], sections = sections)
+    }
   }
   df <- data.table::setDF(
     data.table::rbindlist(linesproc, fill = TRUE, use.names = TRUE)
